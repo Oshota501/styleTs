31b0dbf40cb76d60ef983dad64b04e9280e341fb
Oshota501 <kakidesu1111@gmail.com>
2025-07-02 13:16:00 +0900

    v1

--- web.ts
+++ web.ts
@@ -1,49 +1,47 @@
-
 class BackGroundKishoi{
-    public elm ;
-    private color = [
-        function(flag){
-            if(flag){
-                return "rgba(255, 8, 8, 0.247)";
-            }else{
-                return "rgba(222, 255, 8, 0.25)";
-            }
-        },
-        function(flag){
-            if(flag){
-                return "rgba(90, 255, 8, 0.25)";
-            }else{
-                return "rgba(8, 24, 255, 0.25)";
-            }
-        },
-        function(flag){
-            if(flag){
-                return "rgba(255, 255, 255, 0.25)";
-            }else{
-                return "rgba(0, 0, 0, 0.25)";
-            }
-        },
-
+    public elms ;
+    public color = [
+        "rgba(255, 8, 8, 0.247)",
+        "rgba(222, 255, 8, 0.25)",
+        "rgba(90, 255, 8, 0.25)",
+        "rgba(8, 24, 255, 0.25)",
+        "rgba(255, 255, 255, 0.25)",
+        "rgba(0, 0, 0, 0.25)",
     ] ;
-    private gradient(flag,n,t){
+    private gradient(n,useN){
         let text = "linear-gradient("+n+"deg,";
-        for(let i = 0 ; i < 20 ; i++){
-            text += this.color[t](flag) +",";
-            text += this.color[t](!flag) +",";
+        if(this.f){
+            for(let i = 0 ; i < 20 ; i++){
+                text += this.color[useN*2] +",";
+                text += this.color[useN*2+1] +",";
+            }
+            text += this.color[useN*2];
+        }else{
+            for(let i = 0 ; i < 20 ; i++){
+                text += this.color[useN*2+1] +",";
+                text += this.color[useN*2] +",";
+            }
+            text += this.color[useN*2+1];
         }
-        text += this.color[t](flag);
+        
         text += ")";
         return text ;
     }
     private f = true ;
     public start(){
         setInterval(()=>{
-            this.elm.style.background = this.gradient(this.f,20,0)+","+this.gradient(this.f,80,1)+","+this.gradient(this.f,120,2);
-            if(this.f){
-                this.f=false;
-            }else{
-                this.f=true;
+            for(let i = 0 ; i < this.elms.length ; i++){
+                this.elms[i].style.background = this.gradient(20,0)+","+this.gradient(80,1)+","+this.gradient(120,2);
+                if(this.f){
+                    this.f=false;
+                }else{
+                    this.f=true;
+                }
             }
         },100);
     }
+}
+class TextDenger{
+    public elms ;
+
 }
